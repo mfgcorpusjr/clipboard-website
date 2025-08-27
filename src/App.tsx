72f9717851f2ec7c0240e2fc022babba506a33af
mfgcorpusjr <mcorpus.dev@gmail.com>
2025-08-27 09:13:44 +0800

    snippets section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Hero from "@/components/Hero";
+import Snippets from "@/components/Snippets";
 
 export default function App() {
   return (
     <main>
       <Hero />
+
+      <Snippets />
     </main>
   );
 }
---
+++ src/components/Snippets.tsx
@@ -0,0 +1,13 @@
+export default function Snippets() {
+  return (
+    <div className="section">
+      <h2>Keep track of your snippets</h2>
+
+      <h4>
+        Clipboard instantly stores any item you copy in the cloud, meaning you
+        can access your snippets immediately on all your devices. Our Mac and
+        iOS apps will help you organize everything.
+      </h4>
+    </div>
+  );
+}
